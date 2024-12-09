750d89468ac8116300e7a329d1967942487059d8
Deepak ES <deepakes2003@gmail.com>
2024-12-09 11:43:08 +0530

    wdwwcw

---
+++ src/app/About/Aboutme/layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function lauout({children}) {
+  return (
+    <div>
+      <h1 className='text-3xl font-semibold'>this is about me layout</h1>
+
+      {children}
+    </div>
+  )
+}
+
+export default lauout
---
+++ src/new_file/New_file.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./New_file.module.css"
+function New_file() {
+  return (
+    <div>
+      <h1 className={styles.head}>TEXT</h1>
+    </div>
+  )
+}
+
+export default New_file
